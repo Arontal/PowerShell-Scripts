0d61532f774517fc1b87a766179f8b093d24f6f5
GeoffreyArtis <Geoffrey.f.Artis@gmail.com>
2024-08-30 19:48:00 -0700

    Added Additional Methods and Fucntion

--- Software Validation.ps1
+++ Software Validation.ps1
@@ -4,5 +4,25 @@ Test-path HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ #Software PS
 #Tests whether the software is stored in the x86 directory.
 Test-path HKLM:SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\ #Software PSPath
 
+#Another way to validate software
+get-package -name "Google Chrome" # <-(Software Name in "")
+
+# Discovery of various attributes of a program ex Google Chrome. 
+Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ #Software PSPath"
+Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome"
 
 
+#Function to Loop against multiple assets 
+#List of Active workstations to validate software against in a variable to be called. 
+$workstations = Get-Content C:\Users\Public\Desktop\WS.txt | Where-Object ($_)
+#Credentials should have elevated priviledges to gain access to each system 
+$Creds = Get-Credential
+
+#Foreach will loop through the workstations listed in the WS.txt and output a True or false statement.
+foreach ($ws in $workstations){
+    $session = Enter-PSSession -ComputerName $ws -Credential $Creds
+    Invoke-Command -Session $session -ScriptBlock {
+
+        Test-path "HKLM:SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome" -Verbose 
+    }
+}
